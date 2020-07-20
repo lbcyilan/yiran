680f984a93f3b812e50e0c8e3dfae423c79577b6
黎丙才 <2636712927@qq.com>
2020-07-20 11:13:31 +0800

    reverse反解析（路由）

--- dome/users/urls.py
+++ dome/users/urls.py
@@ -3,6 +3,10 @@ from django.urls import re_path
 
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
     re_path(r'^index/$', views.index),
+    re_path(r'^index1/$', views.index1, name='index1'),
+    re_path(r'^say/$', views.say, name='say'),
 ]
--- dome/users/views.py
+++ dome/users/views.py
@@ -3,9 +3,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# 定义视图函数
+from django.urls import reverse
+
 
 def index(request):
 
-    print('我来了')
+    print('ksks')
+
 
     return HttpResponse('lala')
+# reverse 反解析
+def index1(request):
+    """index1函数"""
+
+    # 获取本函数的路径:
+    url = reverse('user:index1')
+    print(url)      # /users/index
+
+    # 在index1函数中获取say函数的路径
+    url1 = reverse('user:say')
+    print(url1)         # /users/say
+
+
+    return HttpResponse('好的')
+
+def say(request):
+    """say函数"""
+
+    return HttpResponse('say')
