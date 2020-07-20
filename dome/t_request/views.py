46409630870dad44fa8e409c925e7bc4d5790409
黎丙才 <2636712927@qq.com>
2020-07-20 11:29:49 +0800

    获取查询字符串参数

--- dome/t_request/views.py
+++ dome/t_request/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+# 查询字符串传参
+def index(request):
+
+    print(request.GET.get('a'))
+    print(request.GET.get('b'))
+    print(request.GET.getlist('b'))
+
+    return HttpResponse('获取查询字符串参数成功')
